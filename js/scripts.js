c9dd921c0e6d423b4008684076e524a8e561af1d
hillary.dalie@gmail.com <hidalie@gmail.com>
2019-07-29 11:29:54 +0300

    Add submit form from the order form

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
+//Business Logis
+function pizzamenu(size, toppings, crust,){
+this.size = size;
+this.toppings = toppings;
+this.crust = crust;
+}
+
+pizzamenu.prototype.totalCost = function (){
+	return this.size + this.toppings + this.crust
+}
+
+//User Logic
 $(document).ready(function(){
 	//Hover to higlight background with white colour.
 	$("#pizzamenu1,#pizzamenu2,#pizzamenu3,#pizzamenu4,#pizzamenu5,#pizzamenu6,#pizzamenu7,#pizzamenu8").hover(function(){
@@ -5,11 +17,25 @@ $(document).ready(function(){
 	  }, function(){
 	  $(this).css("background-color", "#ffa500e6");
 	});
-	/*
-	//Hover on selected or highlighted item. 
-	$("#pizzamenu1,#pizzamenu2,#pizzamenu3,#pizzamenu4,#pizzamenu5,#pizzamenu6,#pizzamenu7,#pizzamenu8").mouseenter(function(){
-    $("#panel1,#panel2,#panel3,#panel4,#panel5,#panel6,#panel7,#panel8").show("slow");
-	});
-	//On click the active item, bring up form to order.
-	*/
+
+	// This will give our value for the seleced Pizza size
+	var pizzaSize = 0;
+
+	$("form#pizzaorder").submit (function(){
+		for (var j=0; j< document.getElementsByName("pizzaslected").length;j++){
+			if (document.getElementsByName("pizzaslected")[j].checked)
+				pizzaSize = parseInt(document.getElementsByName("pizzaslected")[j].value);
+		}
+	})
+
+	//This will give our value for the selected crust
+	var crustType = 0;
+
+	$("form#crustselected").submit (function(){
+		for (var j=0; i< document.getElementsByName("crustselected").length;i++){
+			if (document.getElementsByName("crustselected")[i].checked)
+				crustType = parseInt(document.getElementsByName("crustselected")[j].value);
+		}
+		alert(crustType);
+	})
 });
